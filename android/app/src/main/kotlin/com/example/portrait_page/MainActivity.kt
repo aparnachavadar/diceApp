2fdb6cece0fb719ae078b349af4490c325cac612
aparnachavadar <aparna.chavadar@gmail.com>
2023-09-08 21:28:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/portrait_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portrait_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/portrait_pg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portrait_pg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
